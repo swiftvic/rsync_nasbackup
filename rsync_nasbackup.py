64b27cfbd2e95f2ad3f3d052a61e8169067496d1
Victor <victor.auyeung@gmail.com>
2020-09-16 22:57:20 -0400

    updated into  functions

--- rsync_nasbackup.py
+++ rsync_nasbackup.py
@@ -1,27 +1,44 @@
 import os, subprocess, time
 
-synologypathhome = "/media/synology/home"
-synologypathshared = "/media/synology/shared"
-seagatepath = "/media/seagate"
+synology_path_home = "/media/synology/home"
+synology_path_shared = "/media/synology/shared"
+seagate_path = "/media/seagate"
 
-paths =[synologypathhome, synologypathshared, seagatepath]
+paths =[synology_path_home, synology_path_shared, seagate_path]
 
 for path in paths:
     print(path)
     
-print("Checking if Synology paths are mounted.")
-if os.path.ismount(synologypathhome):
-    # rsync
-    print("mounted")
-    pass
-else:
-    print(f"{synologypathhome} not mounted, mounting now..")
-    subprocess.check_call(["mount", synologypathhome])
-    print(f"Mounted: {synologypathhome}")
-    time.sleep(10)
+def check_mounts(path):
+    '''
+    Function checks mounts and if the path is not mounted it will attempt to mount it.
+    Will return True if mounted. False if not.
+    '''
+    print("Checking if Synology paths are mounted.")
+    if os.path.ismount(path):
+        # rsync
+        print(f"{path} is already mounted.")
+        return True
+    else:
+        print(f"{path} not mounted, mounting now..")
+        try:
+            subprocess.check_call(["mount", path])
+            print(f"Mounted: {path}")
+            return True
+        except:
+            print(f"Failed to mount {path}")
+            return False
 
-# cleanup
-try: 
-    subprocess.check_call(["umount", "--verbose", synologypathhome])
-except:
-    print("Could not unmount")
+def unmount(path):
+    try: 
+        subprocess.check_call(["umount", "--verbose", path])
+    except:
+        print("Could not unmount")
+
+if __name__ == '__main__':
+    is_mounted = check_mounts(synology_path_home)
+    if is_mounted:
+        time.sleep(10)
+    
+    unmount(synology_path_home)
+    
