f32ca7b953a19b71fdfc222f59c758e566727946
Victor <victor.auyeung@gmail.com>
2020-09-17 22:42:50 -0400

    added functions and rsync commands

--- rsync_nasbackup.py
+++ rsync_nasbackup.py
@@ -1,22 +1,23 @@
-import os, subprocess, time
+import os, subprocess, time, shlex
 
 synology_path_home = "/media/synology/home"
 synology_path_shared = "/media/synology/shared"
-seagate_path = "/media/seagate"
+dest_path = "/media/seagate"
 
-paths =[synology_path_home, synology_path_shared, seagate_path]
+source_paths = [synology_path_home, synology_path_shared]
+rsync = "rsync -avz --progress --delete --exclude '#recycle'"
+#rsync_cmd = f"rsync -avz --progress --delete --exclude '#recycle' {source_paths} {dest_path}" 
 
-for path in paths:
-    print(path)
+for source_path in source_paths:
+    print(source_path)
     
-def check_mounts(path):
+def check_mount(path):
     '''
     Function checks mounts and if the path is not mounted it will attempt to mount it.
     Will return True if mounted. False if not.
     '''
-    print("Checking if Synology paths are mounted.")
+    print(f"Checking if '{path}'' is mounted.")
     if os.path.ismount(path):
-        # rsync
         print(f"{path} is already mounted.")
         return True
     else:
@@ -35,10 +36,22 @@ def unmount(path):
     except:
         print("Could not unmount")
 
+def rsync_cmd(source, dest):
+    return (rsync + " " + source + " " + dest)
+
 if __name__ == '__main__':
-    is_mounted = check_mounts(synology_path_home)
-    if is_mounted:
+    # Check if dest path is mounted
+    if check_mount(dest_path):
+        for source_path in source_paths:
+            is_mounted = check_mount(source_path)
+            if is_mounted:
+                # add to list
+
         time.sleep(10)
+    else:
+        # Cannout mount source, exit
+        print("Source not mounted, exiting.")
+        exit()
     
-    unmount(synology_path_home)
+    unmount(dest_path)
     
